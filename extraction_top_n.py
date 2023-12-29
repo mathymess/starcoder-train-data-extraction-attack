13c2f4d0a88e3743aa33737d3d2264296b515a49
Nikolai Gruzinov <ngruzinov@constructor.university>
2023-12-29 13:36:11 +0100

    Run ruff on extraction_top_n.py

--- extraction_top_n.py
+++ extraction_top_n.py
@@ -18,9 +18,9 @@ def load_tokenizer(model_name: str):
     """
     tokenizer = AutoTokenizer.from_pretrained(model_name)
 
-    # For Autoregressive models, padding on the right would mean the model 
+    # For Autoregressive models, padding on the right would mean the model
     # will receive padded tokens as context, which is not useful during generation
-    tokenizer.padding_side = 'left'
+    tokenizer.padding_side = "left"
     tokenizer.pad_token = tokenizer.eos_token
 
     return tokenizer
@@ -38,12 +38,7 @@ def load_model(model_name: str, device: str):
     return model
 
 
-def calculate_perplexity(
-    input_sentence: str,
-    model: torch.nn.Module,
-    tokenizer: transformers.Tokenizer,
-    device: str
-) -> float:
+def calculate_perplexity(input_sentence: str, model, tokenizer, device: str) -> float:
     """
     Calculate exp(loss), where loss is obtained py passing tokenized input sentence to the model
     with the labels set as the same tokenized input (the shifting of the labels is done internally)
@@ -53,11 +48,13 @@ def calculate_perplexity(
     input_ids = torch.tensor(tokenized.input_ids).to(device)
     with torch.no_grad():
         output = model(input_ids, labels=input_ids)
-    
+
     return torch.exp(output.loss).cpu().item()
 
 
-def calculate_perplexity_sliding(input_sentence, model, tokenizer, device, window_size=50):
+def calculate_perplexity_sliding(
+    input_sentence, model, tokenizer, device, window_size=50
+):
     """
     Calculate min(exp(loss)) over a sliding window
     """
@@ -65,15 +62,23 @@ def calculate_perplexity_sliding(input_sentence, model, tokenizer, device, windo
     input = torch.tensor(tokenized.input_ids).to(device)
     min_perplexity = 100000
     with torch.no_grad():
-        for start_idx in range(input.shape[0]-window_size):
-            input_window = input[start_idx: start_idx+window_size]
+        for start_idx in range(input.shape[0] - window_size):
+            input_window = input[start_idx : start_idx + window_size]
             output = model(input_window, labels=input_window)
             min_perplexity = min(min_perplexity, torch.exp(output.loss))
     return min_perplexity
 
 
 def print_best(
-    metric, samples, metric_name, name1, scores1, name2=None, scores2=None, lower_better=True, n=10
+    metric,
+    samples,
+    metric_name,
+    name1,
+    scores1,
+    name2=None,
+    scores2=None,
+    lower_better=True,
+    n=10,
 ):
     """
     Print the top-n best samples according to the given metric
@@ -86,7 +91,9 @@ def print_best(
     print("Metric Name:", metric_name)
     for i, idx in enumerate(idxs):
         if scores2 is not None:
-            print(f"{i+1}: {name1}={scores1[idx]:.3f}, {name2}={scores2[idx]:.3f}, score={metric[idx]:.3f}")
+            print(
+                f"{i+1}: {name1}={scores1[idx]:.3f}, {name2}={scores2[idx]:.3f}, score={metric[idx]:.3f}"
+            )
         else:
             print(f"{i+1}: {name1}={scores1[idx]:.3f}, , score={metric[idx]:.3f}")
 
@@ -97,15 +104,24 @@ def print_best(
 
 
 def print_best_to_file(
-    outfile, metric, samples, metric_name, name1, scores1, name2=None, scores2=None, lower_better=True, n=100
+    outfile,
+    metric,
+    samples,
+    metric_name,
+    name1,
+    scores1,
+    name2=None,
+    scores2=None,
+    lower_better=True,
+    n=100,
 ):
     """
     Print the top-n best samples according to the given metric to a file
     """
-    original_stdout = sys.stdout # Save a reference to the original standard output
+    original_stdout = sys.stdout  # Save a reference to the original standard output
 
-    with open(outfile, 'a') as f:
-        sys.stdout = f # Change the standard output to the file we created.
+    with open(outfile, "a") as f:
+        sys.stdout = f  # Change the standard output to the file we created.
         print("Metric Name:", metric_name)
 
         if lower_better:
@@ -115,7 +131,9 @@ def print_best_to_file(
 
         for i, idx in enumerate(idxs):
             if scores2 is not None:
-                print(f"{i+1}: {name1}={scores1[idx]:.3f}, {name2}={scores2[idx]:.3f}, score={metric[idx]:.3f}")
+                print(
+                    f"{i+1}: {name1}={scores1[idx]:.3f}, {name2}={scores2[idx]:.3f}, score={metric[idx]:.3f}"
+                )
             else:
                 print(f"{i+1}: {name1}={scores1[idx]:.3f}, , score={metric[idx]:.3f}")
 
@@ -123,7 +141,7 @@ def print_best_to_file(
             print(samples[idx])
             print()
             print()
-        
+
         print()
         print()
         sys.stdout = original_stdout  # Reset the standard output to its original value
@@ -134,10 +152,10 @@ def main(args):
 
     # Load models
     print("Loading models...")
-    TOKENIZER_GPT2 = load_tokenizer_for_causal_lm("gpt2")
-    MODEL_GPT2 = load_model_for_causal_lm("gpt2", device)
+    TOKENIZER_GPT2 = load_tokenizer("gpt2")
+    MODEL_GPT2 = load_model("gpt2", device)
     # MODEL_GPT2_MEDIUM = load_model_for_causal_lm("gpt2-medium", device)
-    MODEL_GPT2_XL = load_model_for_causal_lm("gpt2-xl", device)
+    MODEL_GPT2_XL = load_model("gpt2-xl", device)
     print("GPT2 and GPT2-XL models loaded!")
 
     # number of tokens to generate (from paper)
@@ -156,34 +174,46 @@ def main(args):
         for batch in range(num_batches):
             # Create empty prompts
             prompts = [TOKENIZER_GPT2.eos_token] * args.batch_size
-            inputs = TOKENIZER_GPT2(prompts, return_tensors="pt", padding=True).to(device)
+            inputs = TOKENIZER_GPT2(prompts, return_tensors="pt", padding=True).to(
+                device
+            )
 
             # Batched sequence generation
             generated_sequences = MODEL_GPT2_XL.generate(
-                input_ids = inputs.input_ids,
-                attention_mask = inputs.attention_mask,
-                max_length = seq_len,
-                do_sample = True,
-                top_k = top_k,
-                top_p = 1.0
+                input_ids=inputs.input_ids,
+                attention_mask=inputs.attention_mask,
+                max_length=seq_len,
+                do_sample=True,
+                top_k=top_k,
+                top_p=1.0,
             )
 
-            generated_texts = TOKENIZER_GPT2.batch_decode(generated_sequences, skip_special_tokens=True)
+            generated_texts = TOKENIZER_GPT2.batch_decode(
+                generated_sequences, skip_special_tokens=True
+            )
 
             for text in generated_texts:
                 # Calculate perplexity of GPT-XL, GPT2-Small and GPT2-Medium on each generated text
-                perplexity_gpt2_xl = calculate_perplexity(text, MODEL_GPT2_XL, TOKENIZER_GPT2, device) 
-                perplexity_gpt2 = calculate_perplexity(text, MODEL_GPT2, TOKENIZER_GPT2, device) 
-                # perplexity_gpt2_medium = calculate_perplexity(text, MODEL_GPT2_MEDIUM, TOKENIZER_GPT2, device) 
+                perplexity_gpt2_xl = calculate_perplexity(
+                    text, MODEL_GPT2_XL, TOKENIZER_GPT2, device
+                )
+                perplexity_gpt2 = calculate_perplexity(
+                    text, MODEL_GPT2, TOKENIZER_GPT2, device
+                )
+                # perplexity_gpt2_medium = calculate_perplexity(text, MODEL_GPT2_MEDIUM, TOKENIZER_GPT2, device)
 
                 # Calculate perplexity of GPT-XL on each lower-cased text
-                perplexity_gpt2_xl_lower = calculate_perplexity(text.lower(), MODEL_GPT2_XL, TOKENIZER_GPT2, device) 
+                perplexity_gpt2_xl_lower = calculate_perplexity(
+                    text.lower(), MODEL_GPT2_XL, TOKENIZER_GPT2, device
+                )
 
                 # Calculate Z-lib entropy of sample
-                zlib_entropy = len(zlib.compress(bytes(text, 'utf-8')))
+                zlib_entropy = len(zlib.compress(bytes(text, "utf-8")))
 
                 # Calculate minimum perplexity of GPT2-XL across any sliding window of 50 tokens
-                perplexity_gpt2_xl_window = calculate_perplexity(text.lower(), MODEL_GPT2_XL, TOKENIZER_GPT2, device)
+                perplexity_gpt2_xl_window = calculate_perplexity(
+                    text.lower(), MODEL_GPT2_XL, TOKENIZER_GPT2, device
+                )
 
                 generated_samples.append(text)
                 scores["XL"].append(perplexity_gpt2_xl.cpu())
@@ -224,17 +254,51 @@ def main(args):
     # Show best samples based on Metrics
     # Sort by perplexity of GPT2-XL
     metric = np.log(scores["XL"])
-    print(f"======== top samples by XL perplexity: ========")
-    print_best(metric, generated_samples_clean, "Sort by perplexity of GPT2-XL", "PPL-XL", scores["XL"], lower_better=True)
-    print_best_to_file(args.outfile, metric, generated_samples_clean, "Sort by perplexity of GPT2-XL", "PPL-XL", scores["XL"], lower_better=True)
+    print("======== top samples by XL perplexity: ========")
+    print_best(
+        metric,
+        generated_samples_clean,
+        "Sort by perplexity of GPT2-XL",
+        "PPL-XL",
+        scores["XL"],
+        lower_better=True,
+    )
+    print_best_to_file(
+        args.outfile,
+        metric,
+        generated_samples_clean,
+        "Sort by perplexity of GPT2-XL",
+        "PPL-XL",
+        scores["XL"],
+        lower_better=True,
+    )
     print()
     print()
 
     # Sort by ratio of perplexity of GPT2-XL and GPT2-Small
     metric = np.log(scores["XL"]) / np.log(scores["SMALL"])
-    print(f"======== top samples by ratio of XL and SMALL perplexity: ========")
-    print_best(metric, generated_samples_clean, "Sort by ratio of perplexity of GPT2-XL and GPT2-Small", "PPL-XL", scores["XL"], "PPL-SMALL", scores["SMALL"], lower_better=True)
-    print_best_to_file(args.outfile, metric, generated_samples_clean, "Sort by ratio of perplexity of GPT2-XL and GPT2-Small", "PPL-XL", scores["XL"], "PPL-SMALL", scores["SMALL"], lower_better=True)
+    print("======== top samples by ratio of XL and SMALL perplexity: ========")
+    print_best(
+        metric,
+        generated_samples_clean,
+        "Sort by ratio of perplexity of GPT2-XL and GPT2-Small",
+        "PPL-XL",
+        scores["XL"],
+        "PPL-SMALL",
+        scores["SMALL"],
+        lower_better=True,
+    )
+    print_best_to_file(
+        args.outfile,
+        metric,
+        generated_samples_clean,
+        "Sort by ratio of perplexity of GPT2-XL and GPT2-Small",
+        "PPL-XL",
+        scores["XL"],
+        "PPL-SMALL",
+        scores["SMALL"],
+        lower_better=True,
+    )
     print()
     print()
 
@@ -248,33 +312,97 @@ def main(args):
 
     # Sort by ratio of XL perplexity and ZLIB entropy
     metric = np.log(scores["XL"]) / np.log(scores["ZLIB"])
-    print(f"======== top samples by ratio of XL perplexity and ZLIB entropy: ========")
-    print_best(metric, generated_samples_clean, "Sort by ratio of XL perplexity and ZLIB entropy", "PPL-XL", scores["XL"], "Entropy-Zlib", scores["ZLIB"], lower_better=True)
-    print_best_to_file(args.outfile, metric, generated_samples_clean, "Sort by ratio of XL perplexity and ZLIB entropy", "PPL-XL", scores["XL"], "Entropy-Zlib", scores["ZLIB"], lower_better=True)
+    print("======== top samples by ratio of XL perplexity and ZLIB entropy: ========")
+    print_best(
+        metric,
+        generated_samples_clean,
+        "Sort by ratio of XL perplexity and ZLIB entropy",
+        "PPL-XL",
+        scores["XL"],
+        "Entropy-Zlib",
+        scores["ZLIB"],
+        lower_better=True,
+    )
+    print_best_to_file(
+        args.outfile,
+        metric,
+        generated_samples_clean,
+        "Sort by ratio of XL perplexity and ZLIB entropy",
+        "PPL-XL",
+        scores["XL"],
+        "Entropy-Zlib",
+        scores["ZLIB"],
+        lower_better=True,
+    )
     print()
     print()
 
     # Sort by ratio of perplexity of GPT2-XL on normal and lower-cased sample
     metric = np.log(scores["XL"]) / np.log(scores["LOWER"])
-    print(f"======== top samples by ratio of perplexity of GPT2-XL on normal and lower-cased sample: ========")
-    print_best(metric, generated_samples_clean, "Sort by ratio of perplexity of GPT2-XL on normal and lower-cased sample", "PPL-XL", scores["XL"], "PPL-XL-Lower", scores["LOWER"], lower_better=True)
-    print_best_to_file(args.outfile, metric, generated_samples_clean, "Sort by ratio of perplexity of GPT2-XL on normal and lower-cased sample", "PPL-XL", scores["XL"], "PPL-XL-Lower", scores["LOWER"], lower_better=True)
+    print(
+        "======== top samples by ratio of perplexity of GPT2-XL on normal and lower-cased sample: ========"
+    )
+    print_best(
+        metric,
+        generated_samples_clean,
+        "Sort by ratio of perplexity of GPT2-XL on normal and lower-cased sample",
+        "PPL-XL",
+        scores["XL"],
+        "PPL-XL-Lower",
+        scores["LOWER"],
+        lower_better=True,
+    )
+    print_best_to_file(
+        args.outfile,
+        metric,
+        generated_samples_clean,
+        "Sort by ratio of perplexity of GPT2-XL on normal and lower-cased sample",
+        "PPL-XL",
+        scores["XL"],
+        "PPL-XL-Lower",
+        scores["LOWER"],
+        lower_better=True,
+    )
     print()
     print()
 
     # Sort by minimum perplexity of GPT2-XL on window of size 50
     metric = np.log(scores["WINDOW"])
-    print(f"======== top samples by minimum XL perplexity across a sliding window of size 50: ========")
-    print_best(metric, generated_samples_clean, "Sort by minimum perplexity of GPT2-XL on window of size 50", "PPL-WINDOW", scores["WINDOW"], lower_better=True)
-    print_best_to_file(args.outfile, metric, generated_samples_clean, "Sort by minimum perplexity of GPT2-XL on window of size 50", "PPL-WINDOW", scores["WINDOW"], lower_better=True)
+    print(
+        "======== top samples by minimum XL perplexity across a sliding window of size 50: ========"
+    )
+    print_best(
+        metric,
+        generated_samples_clean,
+        "Sort by minimum perplexity of GPT2-XL on window of size 50",
+        "PPL-WINDOW",
+        scores["WINDOW"],
+        lower_better=True,
+    )
+    print_best_to_file(
+        args.outfile,
+        metric,
+        generated_samples_clean,
+        "Sort by minimum perplexity of GPT2-XL on window of size 50",
+        "PPL-WINDOW",
+        scores["WINDOW"],
+        lower_better=True,
+    )
     print()
     print()
 
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('--N', default=20, type=int, help='Number of samples to generate')
-    parser.add_argument('--batch_size', default=6, type=int, help='Batch size')
-    parser.add_argument('--outfile', type=str, help='Output file to log top samples based on each metric')
+    parser.add_argument(
+        "--N", default=20, type=int, help="Number of samples to generate"
+    )
+    parser.add_argument("--batch_size", default=6, type=int, help="Batch size")
+    parser.add_argument(
+        "--outfile",
+        type=str,
+        help="Output file to log top samples based on each metric",
+    )
 
     args = parser.parse_args()
 
